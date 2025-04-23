99816c0748bc85aa07b27186a8f626658ec96e29
Akshay Joshi <akshayjoshi56@gmail.com>
2025-04-23 19:10:46 +0000

    Updated constants & requirements file

--- coding_task/constants.py
+++ coding_task/constants.py
@@ -2,8 +2,12 @@
 import os
 
 # constants for file paths
+ROOT_DIR: str = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'coding_task')
 DATA_DIR: str = os.path.join(os.path.dirname(__file__), "data")
 LOG_DIR: str = os.path.join(os.path.dirname(__file__), "logs")
-ROOT_DIR: str = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'coding_task')
+EVAL_DIR: str = os.path.join(os.path.dirname(__file__), "evaluation")
+PLOT_DIR: str = os.path.join(os.path.dirname(__file__), "evaluation", "plots")
 os.makedirs(DATA_DIR, exist_ok=True)
 os.makedirs(LOG_DIR, exist_ok=True)
+os.makedirs(EVAL_DIR, exist_ok=True)
+os.makedirs(PLOT_DIR, exist_ok=True)
