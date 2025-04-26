180e82e21c62966942cea9ea2d33fd3ab0bcaa98
Akshay Joshi <akshayjoshi56@gmail.com>
2025-04-26 20:49:17 +0000

    disabled one of the boiler plate code

--- coding_task/server/intent_classifier.py
+++ coding_task/server/intent_classifier.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
+""" 
+Instead of this file, we are using main.py & predictor.py in the Inference directory
+to implement the Torch classes/functions for model inference. This file is kept here for reference and to avoid breaking the code structure.
+"""
+
 import json
 
 
